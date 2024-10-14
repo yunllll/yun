9424a09e4a9ceab68da8a0426035cf23eb3af681
76074@Rabbit <76074@Rabbit>
2024-10-15 00:03:14 +0800

    6666

--- src/views/element.vue
+++ src/views/element.vue
@@ -15,62 +15,19 @@ const dplayerRef = ref(null);
 onMounted(() => {
     const dp = new DPlayer({
         container: dplayerRef.value,
-        autoplay: false,
-        theme: '#FADFA3',
-        loop: true,
-        lang: 'zh-cn',
         screenshot: true,
-        hotkey: true,
-        preload: 'auto',
-        logo: 'logo.png',
-        volume: 0.7,
-        mutex: true,
         video: {
-            url: 'dplayer.mp4',
-            pic: 'dplayer.png',
-            thumbnails: 'thumbnails.jpg',
-            type: 'auto',
+            url: 'https://www.dmttang.com/bfe37917-1935-43f6-b22b-6e723f20ba82', // 替换为你的视频文件路径
+            pic: '../assets/pic.jpg', // 替换为你的视频封面图路径
+            thumbnails: 'thumbnails.jpg', // 替换为你的视频缩略图路径
         },
         subtitle: {
-            url: 'dplayer.vtt',
-            type: 'webvtt',
-            fontSize: '25px',
-            bottom: '10%',
-            color: '#b7daff',
+            url: 'webvtt.vtt', // 替换为你的视频字幕文件路径
         },
         danmaku: {
-            id: '9E2E3368B56CDBB4',
+            id: 'demo',
             api: 'https://api.prprpr.me/dplayer/',
-            token: 'tokendemo',
-            maximum: 1000,
-            addition: ['https://api.prprpr.me/dplayer/v3/bilibili?aid=4157142'],
-            user: 'DIYgod',
-            bottom: '15%',
-            unlimited: true,
-            speedRate: 0.5,
         },
-        contextmenu: [
-            {
-                text: 'custom1',
-                link: 'https://github.com/DIYgod/DPlayer',
-            },
-            {
-                text: 'custom2',
-                click: (player) => {
-                    console.log(player);
-                },
-            },
-        ],
-        highlight: [
-            {
-                text: 'marker for 20s',
-                time: 20,
-            },
-            {
-                text: 'marker for 2mins',
-                time: 120,
-            },
-        ],
     });
 });
 
